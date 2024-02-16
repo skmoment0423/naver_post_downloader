c0ab58f286ede753131079920144727916ce020c
DESKTOP-CP8A2HI\apple <skmoment0423@gmail.com>
2024-02-16 23:51:17 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+import re
+from json import loads as loadjson
+from pathlib import Path
+from sys import version_info
+from urllib.parse import unquote, urlparse
+import streamlit as st
+import aiohttp
+
+
+async def queue_downloads(url):
+    title = urlparse(url).query.split('volumeNo=')[1].split('&')[0]
+    desired_path = Path.cwd() / title
+    desired_path.mkdir(parents=False, exist_ok=True)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url) as response:
+            text = await response.text()
+            for item in text.split("data-linkdata=\'")[1:]:
+                linkdata = loadjson(item.split("\'>\n")[0])
+                if 'src' in linkdata:
+                    picture_url = linkdata['src']
+                    picture_id = unquote(urlparse(picture_url).path.split('/')[-1])
+                    picture_name = re.sub('[<>:\"/|?*]', ' ', picture_id).strip()
+                    picture_path = desired_path / picture_name
+                    if not picture_path.is_file():
+                        await download(session, picture_url, picture_path)
+                else:
+                    print(f"Error string does not include 'src' {linkdata}")
+
+
+async def download(session, picture_url, picture_path):
+    async with session.get(picture_url) as r:
+        if r.status == 200:
+            picture_path.write_bytes(await r.read())
+            print(f'Downloaded {picture_url}')
+        else:
+            print(f'Error {r.status} while getting request for {picture_url}')
+
+
+def main():
+    st.title("NAVER POST DOWNLOADER")
+    post_url = st.text_input("네이버 포스트 URL을 입력하세요")
+    if post_url:
+        if st.button("다운로드"):
+            asyncio.run(queue_downloads(post_url))
+    
+    
+if __name__ == '__main__':
+    assert version_info >= (3, 7), 'Script requires Python 3.7+.'
+    main()
